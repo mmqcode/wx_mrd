df09a42c876b01c8e35b8854b31c4808058eb4f7
Derry_Git <m_minqiang@163.com>
2016-10-20 16:12:13 +0800

    Signed-off-by: Derry_Git <m_minqiang@163.com>

--- src/main/java/com/gxgrh/wechat/tools/Constants.java
+++ src/main/java/com/gxgrh/wechat/tools/Constants.java
@@ -1,6 +1,6 @@
 package com.gxgrh.wechat.tools;
 
-/**常量类，集中存储微信项目用到的常量，项目中用到的全局常量最好在本类配置。
+/**常量类，集中存储微信项目用到的常量，项目中用到的全局常量最好在本类配置,其他常量可以在各个类中自己配置。
  * Created by Administrator on 2016/9/26.
  */
 public class Constants {
